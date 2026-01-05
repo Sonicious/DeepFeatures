e32dceac104b7c494f077e0741583b4f8a501064
julia310 <JPeters92@protonmail.com>
2026-01-05 15:39:02 +0100

    clean training script

--- train.py
+++ train.py
@@ -2,14 +2,12 @@ import torch
 import pickle
 import random
 import lightning.pytorch as L
-from model import TransformerAE
-from dataset import HDF5Dataset
-from torch.utils.data import DataLoader, ConcatDataset, Subset
-from torch.utils.data import random_split
-from ml4xcube.preprocessing import get_statistics, get_range
+from model.model import TransformerAE
+from dataset.dataset import HDF5Dataset
+from torch.utils.data import DataLoader
+
 from lightning.pytorch.callbacks import ModelCheckpoint
 from lightning.pytorch.callbacks import EarlyStopping
-from initialize import hierarchical_initialize_weights
 
 torch.set_float32_matmul_precision('medium')  # For better performance
 
@@ -30,49 +28,21 @@ def main():
     batch_size = 16
 
     train_dataset = HDF5Dataset("train.h5")
-    train_dataset_heinich = HDF5Dataset("train_data_11_15_15.h5")
-    val_dataset = HDF5Dataset("test.h5")
-    val_dataset_heinich = HDF5Dataset("val_data_11_15_15.h5")
+    val_dataset = HDF5Dataset("val.h5")
 
-    # Randomly select samples from train_dataset_heinich
-    train_indices = random.sample(range(len(train_dataset_heinich)), 4092)
-    selected_train_subset = Subset(train_dataset_heinich, train_indices)
-
-    # Randomly select samples from val_dataset_heinich
-    val_indices = random.sample(range(len(val_dataset_heinich)), 1364)
-    selected_val_subset = Subset(val_dataset_heinich, val_indices)
-
-    # Concatenate the selected subsets with the original datasets
-    combined_train_dataset = ConcatDataset([train_dataset, selected_train_subset])
-    combined_val_dataset = ConcatDataset([val_dataset, selected_val_subset])
-
-
-    #train_size = int(0.75 * len(train_dataset))
-    #remain_size = len(val_dataset) - train_size
-
-    #val_size = int(0.2 * len(val_dataset))
-    #test_size = len(val_dataset) - val_size
-
-    # Randomly split into validation and test
-    #val_split, test_split = random_split(val_dataset, [val_size, test_size])
-    #train_split, _ = random_split(val_dataset, [train_size, remain_size])
 
     # Create DataLoaders
-    val_iterator   = DataLoader(combined_val_dataset, batch_size=batch_size, shuffle=False, num_workers=32)
-    #test_iterator  = DataLoader(test_split, batch_size=batch_size, shuffle=False, num_workers=32)
-    train_iterator = DataLoader(combined_train_dataset, batch_size=batch_size, shuffle=True, num_workers=32)
+    val_iterator   = DataLoader(train_dataset, batch_size=batch_size, shuffle=False, num_workers=32)
+    train_iterator = DataLoader(val_dataset, batch_size=batch_size, shuffle=True, num_workers=32)
 
     # Initialize the autoencoder model
     autoencoder = TransformerAE()
-    #autoencoder.apply(hierarchical_initialize_weights)
     autoencoder.to(device)  # Move the model to GPU 3
-    #autoencoder.reset_parameters()
 
     # Define checkpointing (optional)
     checkpoint_callback = ModelCheckpoint(
         monitor='val_loss',
         dirpath='checkpoints/',
-        #filename='foundation-ae-{epoch:02d}-{val_loss:.3e}',
         filename='ae-{epoch:02d}-{val_loss:.3e}',
         save_top_k=3,
         mode='min'
